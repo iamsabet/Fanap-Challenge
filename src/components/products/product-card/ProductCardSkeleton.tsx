7cbe9e8338bc9bda25c6e0fa3e0808112e27a2a7
iamsabet <iamsabet@gmail.com>
2025-05-15 09:16:27 +0330

    got rid of useless codes

--- src/components/products/product-card/ProductCardSkeleton.tsx
+++ src/components/products/product-card/ProductCardSkeleton.tsx
@@ -1,8 +1,3 @@
-
-"use client"
-
-import Link from "next/link"
-import { clearProduct } from "@/lib/store/product.store"
 import { Skeleton } from "@/components/ui/skeleton"
 import ProductCardInfoSkeleton from "./ProductCardInfoSkeleton"
 
@@ -11,14 +6,12 @@ const ProductCardSkeleton = () => {
         <article className="relative shadow-sm transition-all duration-150 ease-linear rounded-[12px]
         flex flex-col items-center justify-start gap-y-1 pb-2 max-sm:pb-1.5
         bg-muted mx-1 h-full group">
-            <Link onClick={(() => {
-                clearProduct()
-            })} className="w-full h-full relative" href={`#`}>
+            <div className="w-full h-full relative">
                 <Skeleton
                     className="mx-auto w-[200-px] h-[200px] relative rounded-[12px] bg-muted-foreground opacity-60"
                 />
                 <ProductCardInfoSkeleton />
-            </Link>
+            </div>
         </article>
     )
 }
