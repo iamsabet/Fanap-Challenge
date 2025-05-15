60662947d83e40af3dca5e3feb9d1a46001deefb
iamsabet <iamsabet@gmail.com>
2025-05-15 09:33:22 +0330

    fixes

--- src/components/product/ProductImages.tsx
+++ src/components/product/ProductImages.tsx
@@ -13,10 +13,10 @@ const ProductImages = ({ product }: { product: ProductItemType }) => {
                 className="aspect-square rounded-sm bg-card overflow-hidden object-contain mx-auto"
                 src={product.thumbnail}
                 alt={product.thumbnail + " Image "}
-                width={400}
-                height={400}
+                width={320}
+                height={320}
                 loading='eager'
-                decoding='async'
+                decoding='sync'
                 priority
             />
                 : <Skeleton className={`mx-auto sm:w-[400px] sm:h-[400px] aspect-square w-full rounded-md bg-sidebar-border`} />
